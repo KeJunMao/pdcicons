e5dabd974957f2770a693efc7651e1e9cca57868
KeJun <kejun1997@gmail.com>
2018-03-25 09:00:20 +0800

    style(layout): fix bgcolor

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,10 @@
-document.addEventListener("scroll",function(){
-    var e=window.scrollY>328;
-    document.body.classList.toggle("si-fix",e)
-})
+var $$ = mdui.JQ;
+$$(function(){
+    document.addEventListener("scroll",function(){
+        var e=window.scrollY>328;
+        document.body.classList.toggle("si-fix",e)
+    });
+    document.getElementById('is_dark').addEventListener('click',function(){
+        document.body.classList.toggle("mdui-theme-layout-dark");
+    })
+});
